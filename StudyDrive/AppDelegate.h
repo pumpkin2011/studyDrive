0b3f4ecd506b4ded9786024579fee841c3d8e491
pumpkin <zhenhuanlee@outlook.com>
2017-07-20 07:17:09 +0800

    Initial Commit

---
+++ StudyDrive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyDrive
+//
+//  Created by Aico on 7/20/17.
+//  Copyright © 2017 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StudyDrive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StudyDrive
+//
+//  Created by Aico on 7/20/17.
+//  Copyright © 2017 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
